e921c60fe530469d85d070d50c9bd2a4d613c9b3
Fahad Hussain <fahad_hussain2@hotmail.com>
2016-06-30 19:12:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function sum(firstnum, secnum) {
+    var total = 0;
+    return firstnum + secnum;
+}
+console.log(sum(10, 20));
---
+++ main.ts
@@ -0,0 +1,18 @@
+
+function sum(firstnum:number, ...restparams:number[]):number {
+    var total=0;
+     return firstnum + "" + restparamsp[];     
+  
+    
+}
+
+
+
+
+
+console.log(sum(10,20));
+
+
+
+
+
